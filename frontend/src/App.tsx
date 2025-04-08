d9ebf8461366420931fd80678a70fb79be03e105
Yurii <yurii.rudenko26@gmail.com>
2025-04-08 19:26:08 +0300

    added-home-components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './styles/main.css'
 import Home from './Pages/Home/Home';
+import Menu from './Components/Small/Menu/Menu';
 
 function App() {
   return (
 
     <div className="App">
+      <Menu />
       <Home />
     </div>
 
